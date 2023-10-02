cff3d755f11350b66a896ca554bdfa99f2913f49
DianaAlibekova <da3002@nyu.edu>
2023-10-02 22:29:48 +0400

    HW Illustrator

---
+++ script.js
@@ -0,0 +1,13 @@
+let clickButton = document.getElementById("interactivity")
+
+clickButton.addEventListener("click", () => {
+  // document.getElementById("textbox").innerText = "CENTRAL ASIA!"
+let textbox = document.getElementById("textbox");
+ textbox.innerText = "CENTRAL ASIA!";
+ textbox.style.fontSize = "100px";
+ textbox.style.color = "blue";
+ textbox.style.textAlign = "center";
+ textbox.style.display = "flex";
+ textbox.style.alignItems = "center";
+ textbox.style.justifyContent = "center"; 
+})
